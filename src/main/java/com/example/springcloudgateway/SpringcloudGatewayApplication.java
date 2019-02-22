dcd6d68bc32edbb0c07f1f05572eb836ead48ae3
liwei <805717605@qq.com>
2019-02-22 21:06:08 +0800

    Initial commit

---
+++ src/main/java/com/example/springcloudgateway/SpringcloudGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.example.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudGatewayApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SpringcloudGatewayApplication.class, args);
+    }
+}
