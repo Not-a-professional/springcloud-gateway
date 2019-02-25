40f90d6b73d146c87e79b23d5a6618b1d2abbca9
liwei <805717605@qq.com>
2019-02-25 20:00:31 +0800

    测试api的路由转发，并配置为与服务发现连用的方式进行调用

---
+++ src/main/java/com/example/springcloudgateway/Routes/TestRoute.java
@@ -0,0 +1,16 @@
+//package com.example.springcloudgateway.Routes;
+//
+//import org.springframework.cloud.gateway.route.RouteLocator;
+//import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+//import org.springframework.context.annotation.Bean;
+//
+//public class TestRoute {
+//    @Bean
+//    public RouteLocator testRoute(RouteLocatorBuilder routeLocatorBuilder) {
+//        return routeLocatorBuilder.routes()
+//                .route(
+//                        r -> r.uri("http://106.15.185.93:3080/")
+//                )
+//                .build();
+//    }
+//}
